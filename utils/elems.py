376c78c719a91da738fc0e73270e6b82acaf8b41
GreenGrape-g <pio3434@naver.com>
2024-10-18 16:21:31 +0900

    1018

--- utils/elems.py
+++ utils/elems.py
@@ -1,7 +1,7 @@
 # elems.py
 
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.tools.tavily_search import TavilySearchResults
